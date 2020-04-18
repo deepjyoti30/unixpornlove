f4f27cea3ad38fb794d32b7e044275c3d058ee84
deepjyoti30 <deep.barman30@gmail.com>
2020-04-18 18:33:29 +0530

    update: Made the top post dynamic.

--- frontend/js/fetch.js
+++ frontend/js/fetch.js
@@ -1,7 +1,8 @@
 var fetchTrending = new Vue({
-    el: "#trending",
+    el: "#body",
     data: {
         trendingPostsContainer: [],
+        topPost: null,
         topURL: 'https://www.reddit.com/r/unixporn/top/.json?count=20'
     },
     methods: {
@@ -14,14 +15,27 @@ var fetchTrending = new Vue({
                 .then(response => {return response.json()})
                 .then((jsonData) => {
                     // Parse the data and store it in the container
-                    this.trendingPostsContainer = jsonData["data"]["children"]
+                    let data = jsonData["data"]["children"]
+                    this.topPost = data[0]
+                    data.splice(0, 1)
+                    this.trendingPostsContainer = data
                 })
+        },
+        calcPercentage(data) {
+            return (data.ups / (data.ups + data.downs) * 100)
+        },
+        getFullUri(permalink) {
+            return "https://www.reddit.com/" + permalink
         }
     },
     computed: {
         getPosts: function() {
-            return this.trendingPostsContainer;
-        }
+            return this.trendingPostsContainer
+        },
+        getTopPost: function() {
+            if (this.topPost)
+                return this.topPost
+        },
     },
     mounted() {
         this.fetchData()
