aaa0dfad5a7858f491da649b6e491e975da275c3
deepjyoti30 <deep.barman30@gmail.com>
2020-04-20 19:37:24 +0530

    small bug fix.

--- frontend/js/fetch.js
+++ frontend/js/fetch.js
@@ -294,6 +294,7 @@ var fetchTrending = new Vue({
                 },
                 onClose: () => {
                     this.resetTitleUrl()
+                    this.showModalComment = false
                 },
                 disableScroll: true,
                 awaitCloseAnimation: true,
